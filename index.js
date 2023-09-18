481f8798744aacd45ba846098f284be9af9b07ba
Kapil-Badgujjar <k.b.cool.boy99@gmail.com>
2023-09-18 14:11:22 +0530

    Optimized code and fixed env file bug

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ import tasks from './router/tasks.js';
 import comments from './router/comments.js';
 import authenticateUser from './middlewares/authenticateMiddleware.js';
 const server = express();
+const port = process.env.SERVER_ADDRESS || 7171;
+
 const allowedOrigins = [process.env.CLIENT_ADDRESS];
-const port = process.env.PORT || 7171;
 
 server.use(cors({
     origin: (origin, callback) => {
@@ -28,7 +29,6 @@ server.use('/users', users);
 server.use('/tasks', tasks);
 server.use('/comments', comments);
 
-// Get user details from token
 server.get('/getuserdetails', authenticateUser, (req, res)=>{
     const data = { id: req.user.id, username: req.user.username, email_id: req.user.email_id, role: req.user.role}
     res.status(200).send({token: req.user.newToken|| undefined, data, message: 'Already logged in'});
